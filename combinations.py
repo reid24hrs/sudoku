ab4565f4ab4e65c317bf0dda03b971727debe5eb
Reid <reid@macbook-2.local>
2018-08-18 13:44:09 +0100

    added repeat loop to code

--- combinations.py
+++ combinations.py
@@ -2,26 +2,33 @@
 # n digits in the set n E {1-9} where each n digit does not repeat 
 # that add up to the total s
 
-import itertools as iter
+def combinations():
+   
+   import itertools as iter
+   
+   n = input("Enter length of combination: ")
+   s = input("Enter the total value the combination must add up to: ")
+   sequence_length = 9 # every line in Sudoku is always 9 values long
+   
+   x = list(range(1,10)) # create a list of the numbers 1-9
+   
+   # create the matrix of combinations of n digits
+   
+   combinations = list(iter.combinations(x, n))
+   # print(combinations)
+   
+   solutions = [] # create an empty list to append valid solutions
 
-n = input("Enter length of combination: ")
-s = input("Enter the total value the combination must add up to: ")
-sequence_length = 9 # every line in Sudoku is always 9 values long
+   for i in range(len(combinations)):
+      if sum(combinations[i]) == s:
+         solutions.append(combinations[i]) 
+   
+   print(solutions)
 
-x = list(range(1,10)) # create a list of the numbers 1-9
-
-# create the matrix of combinations of n digits
-
-combinations = list(iter.combinations(x, n))
-# print(combinations)
-
-solutions = [] # create an empty list to append valid solutions
-
-for i in range(len(combinations)):
-   if sum(combinations[i]) == s:
-      solutions.append(combinations[i]) 
-
-print(solutions)
-
+# main part of the programme
+loop = "y"
+while loop == "y":
+   combinations()
+   loop = raw_input("Enter y to try again: ")
 
 
